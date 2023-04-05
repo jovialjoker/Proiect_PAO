3114001d9539a7997ea7ad138e9e255c69ee35f7
Florea George <florgeo_02@yahoo.com>
2023-04-05 22:51:44 +0300

    Workout + Split Create and Read functionality

--- project/src/Main.java
+++ project/src/Main.java
@@ -36,31 +36,93 @@ public class Main {
                     } else {
                         switch (option) {
                             case "2" -> {
-                                System.out.println("not implemented 2");
+                                Split newSplit = new Split();
+                                System.out.println("\nCreate your new split:\n");
+                                System.out.println("\nName:");
+                                newSplit.setName(sc.nextLine());
+                                System.out.println("\nChoose the workouts:");
+                                List<Workout> workoutList = _splitController.GetWorkouts();
+                                if (workoutList.size() != 0) {
+                                    do {
+                                        int i = 1;
+                                        for (Workout workout : workoutList) {
+                                            System.out.println("\n" + i + ", " + workout.getName());
+                                            List<Exercise> exerciseList = workout.getExercises();
+                                            for(Exercise exercise: exerciseList){
+                                                System.out.println("- "+ exercise.getName() + ": ");
+                                                if (exercise.getClass() == Bodybuilding_Exercise.class) {
+                                                    System.out.println("\t" + ((Bodybuilding_Exercise) exercise).getRepsNo() + " reps;");
+                                                    System.out.println("\t" + ((Bodybuilding_Exercise) exercise).getWeight() + " kgs;");
+                                                } else if (exercise.getClass() == Cardio_Exercise.class) {
+                                                    System.out.println("\t" + ((Cardio_Exercise) exercise).getTime() + " mins;");
+                                                    System.out.println("\t" + ((Cardio_Exercise) exercise).getDistance() + " kms;");
+                                                }
+                                            }
+                                            i++;
+                                        }
+
+                                        option = sc.nextLine();
+                                        try {
+                                            int index = Integer.parseInt(option) - 1;
+                                            Workout workout = _splitController.GetWorkout(workoutList.get(index).getIdWorkout());
+                                            List<Workout> oldWorkouts = newSplit.getWorkouts();
+                                            oldWorkouts.add(workout);
+                                            newSplit.setWorkouts(oldWorkouts);
+                                        } catch (Exception e) {
+
+                                        }
+                                        System.out.println("\n Do you want to continue adding workouts? [y/n]");
+                                        option = sc.nextLine();
+                                    } while (!option.equals("n"));
+
+                                    _splitController.AddSplit(newSplit);
+                                    System.out.println("\n New split created succesfully!");
+                                } else {
+                                    System.out.println("\n There are no workouts! Add a workout then come back!");
+                                }
                                 break;
                             }
                             case "3" -> {
-//                                Workout newWorkout = new Workout();
-//                                System.out.println("\nCreate your new Workout:\n");
-//                                System.out.println("\nName:");
-//                                newExercise.setName(sc.nextLine());
-//                                System.out.println("\nDescription:");
-//                                newExercise.setDescr(sc.nextLine());
-//
-//                                System.out.println("\n Choose the muscle group targeted:");
-//                                for(Muscle_Groups value: Muscle_Groups.values()){
-//                                    System.out.println("\n" + value.getLabel() + ": " + value.name());
-//                                }
-//                                option = sc.nextLine();
-//                                try {
-//                                    int index = Integer.parseInt(option);
-//                                    Muscle_Groups mg = Arrays.stream(Muscle_Groups.values()).filter(e -> e.getLabel() == index).findFirst().get();
-//                                    newExercise.setMuscleGroup(mg);
-//                                } catch (Exception e) {
-//
-//                                }
-//                                _splitController.AddExercise(newExercise);
-                                System.out.println("\n New exercise created succesfully!");
+                                Workout newWorkout = new Workout();
+                                System.out.println("\nCreate your new Workout:\n");
+                                System.out.println("\nName:");
+                                newWorkout.setName(sc.nextLine());
+                                System.out.println("\nChoose the exercise:");
+                                List<Exercise> exerciseList = _splitController.GetExercises();
+                                if (exerciseList.size() != 0) {
+                                    do {
+                                        int i = 1;
+                                        for (Exercise exercise : exerciseList) {
+                                            System.out.println("\n" + i + ". " + exercise.getName());
+                                            if (exercise.getClass() == Bodybuilding_Exercise.class) {
+                                                System.out.println("\t" + ((Bodybuilding_Exercise) exercise).getRepsNo() + " reps;");
+                                                System.out.println("\t" + ((Bodybuilding_Exercise) exercise).getWeight() + " kgs;");
+                                            } else if (exercise.getClass() == Cardio_Exercise.class) {
+                                                System.out.println("\t" + ((Cardio_Exercise) exercise).getTime() + " mins;");
+                                                System.out.println("\t" + ((Cardio_Exercise) exercise).getDistance() + " kms;");
+                                            }
+                                            i++;
+                                        }
+
+                                        option = sc.nextLine();
+                                        try {
+                                            int index = Integer.parseInt(option) - 1;
+                                            Exercise ex = _splitController.GetExercise(exerciseList.get(index).getId());
+                                            List<Exercise> oldExercises = newWorkout.getExercises();
+                                            oldExercises.add(ex);
+                                            newWorkout.setExercises(oldExercises);
+                                        } catch (Exception e) {
+
+                                        }
+                                        System.out.println("\n Do you want to continue adding exercises? [y/n]");
+                                        option = sc.nextLine();
+                                    } while (!option.equals("n"));
+
+                                    _splitController.AddWorkout(newWorkout);
+                                    System.out.println("\n New workout created succesfully!");
+                                } else {
+                                    System.out.println("\n There are no exercises! Add a exercise then come back!");
+                                }
                                 break;
                             }
                             case "4" -> {
@@ -146,11 +208,72 @@ public class Main {
                     } else {
                         switch (option) {
                             case "2": {
-                                System.out.println("not implemented 2");
+                                List<Split> splits = _splitController.GetSplits();
+                                if (splits.size() != 0) {
+                                    do {
+                                        int i = 1;
+                                        for (Split split : splits) {
+                                            System.out.println("\n " + i + ". " + split.getName());
+                                            i++;
+                                        }
+                                        System.out.println("\n If you want to see more info about a split type the number associated with it");
+                                        option = sc.nextLine();
+                                        try {
+                                            int index = Integer.parseInt(option) - 1;
+                                            Split split = _splitController.GetSplit(splits.get(index).getIdSplit());
+                                            System.out.println("\n Your split is: " + split.getName());
+                                            System.out.println("\n The workouts included are: ");
+                                            int j = 1;
+                                            for (Workout workout: split.getWorkouts()) {
+                                                System.out.println(j + workout.getName());
+
+                                                for(Exercise exercise: workout.getExercises()){
+                                                    System.out.println("\t-" + exercise.getName());
+                                                    if (exercise.getClass() == Bodybuilding_Exercise.class) {
+                                                        System.out.println("\t\t" + ((Bodybuilding_Exercise) exercise).getRepsNo() + " reps;");
+                                                        System.out.println("\t\t" + ((Bodybuilding_Exercise) exercise).getWeight() + " kgs;");
+                                                    } else if (exercise.getClass() == Cardio_Exercise.class) {
+                                                        System.out.println("\t\t" + ((Cardio_Exercise) exercise).getTime() + " mins;");
+                                                        System.out.println("\t\t" + ((Cardio_Exercise) exercise).getDistance() + " kms;");
+                                                    }
+                                                    j++;
+                                                }
+                                            }
+                                        } catch (Exception e) {
+
+                                        }
+                                    } while (!option.equals("stop") && !option.equals("back"));
+                                } else {
+                                    System.out.println("\n There are no workouts! Create some workouts and come back!");
+                                }
                                 break;
                             }
                             case "3": {
-                                System.out.println("not implemented 3");
+                                List<Workout> workouts = _splitController.GetWorkouts();
+                                if (workouts.size() != 0) {
+                                    do {
+                                        int i = 1;
+                                        for (Workout workout : workouts) {
+                                            System.out.println("\n " + i + ". " + workout.getName());
+                                            i++;
+                                        }
+                                        System.out.println("\n If you want to see more info about a workout type the number associated with it");
+                                        option = sc.nextLine();
+                                        try {
+                                            int index = Integer.parseInt(option) - 1;
+                                            Workout workout = _splitController.GetWorkout(workouts.get(index).getIdWorkout());
+                                            System.out.println("\n Your workout is: " + workout.getName());
+                                            System.out.println("\n The exercises included are: ");
+                                            for (Exercise exercise : workout.getExercises()) {
+                                                System.out.println("\t " + exercise.getName());
+                                            }
+                                        } catch (Exception e) {
+
+                                        }
+                                    } while (!option.equals("stop") && !option.equals("back"));
+                                } else {
+                                    System.out.println("\n There are no workouts! Create some workouts and come back!");
+                                }
                                 break;
                             }
                             case "4": {
@@ -182,7 +305,7 @@ public class Main {
                                         }
                                     } while (!option.equals("stop") && !option.equals("back"));
                                 } else {
-                                    System.out.println("\n There are no users! Create some users and come back!");
+                                    System.out.println("\n There are no exercises! Create some exercises and come back!");
                                 }
                                 break;
                             }
